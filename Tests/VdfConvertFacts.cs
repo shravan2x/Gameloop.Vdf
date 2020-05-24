d47f50904237eec9b43fef8dc519ef25e3ca1385
Shravan Rajinikanth <mail@shrav.me>
2020-05-24 15:35:57 -0700

    Added test for comment parsing feature

--- Tests/VdfConvertFacts.cs
+++ Tests/VdfConvertFacts.cs
@@ -12,5 +12,37 @@ namespace Tests
         {
             Assert.Throws<VdfException>(() => VdfConvert.Deserialize(String.Empty));
         }
+
+        [Fact]
+        public void CommentsDeserializeCorrectly()
+        {
+            string vdf = @"
+                // Comment type A (at the start of the file)
+                ""root""
+                {
+                    // Comment type B (as a child to an object)
+                    key1 ""value1""
+                    ""key2"" // Comment type C (to the right of a property name)
+                    {
+                        ""key3"" ""value3"" // Comment type D (to the right of a property value)
+                    }
+                }
+                // Comment type E (at the end of the file)
+            ";
+            VProperty result = VdfConvert.Deserialize(vdf);
+
+            VProperty expected = new VProperty("root", new VObject
+            {
+                VValue.CreateComment(" Comment type B (as a child to an object)"),
+                new VProperty("key1", new VValue("value1")),
+                new VProperty("key2", new VObject
+                {
+                    new VProperty("key3", new VValue("value3")),
+                    VValue.CreateComment(" Comment type D (to the right of a property value)"),
+                }),
+            });
+
+            Assert.True(VToken.DeepEquals(result, expected));
+        }
     }
 }
